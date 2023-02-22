91d9958dc5dd9c7ffe2299faf4f177cd728319c6
Julian Waugh <julianwaugh@Julians-MacBook-Air.local>
2023-02-22 11:47:09 -0500

    adding data

--- allcause/data.py
+++ allcause/data.py
@@ -1,12 +1,13 @@
-import pandas as pd
-
-import httplib2
-from bs4 import BeautifulSoup, SoupStrainer
 from pathlib import Path
 
+import httplib2
+import pandas as pd
+from bs4 import BeautifulSoup, SoupStrainer
 
-DATALINK = 'https://www.nber.org/research/data/mortality-data-vital-statistics-nchs-multiple-cause-death-data'
-def download_year_mortality_data(year:int) -> pd.DataFrame:
+DATALINK = "https://www.nber.org/research/data/mortality-data-vital-statistics-nchs-multiple-cause-death-data"
+
+
+def download_year_mortality_data(year: int) -> pd.DataFrame:
     """
     Downloads a year's worth of data from the internet
 
@@ -23,7 +24,7 @@ def download_year_mortality_data(year:int) -> pd.DataFrame:
     """
 
     if year == 2021:
-        id_string= 'mort2021us.csv'
+        id_string = "mort2021us.csv"
     elif year >= 2018:
         id_string = f"Mort{year}US.PubUse.csv"
     else:
@@ -32,13 +33,13 @@ def download_year_mortality_data(year:int) -> pd.DataFrame:
     http = httplib2.Http()
     status, response = http.request(DATALINK)
 
-    for link in BeautifulSoup(response, parse_only=SoupStrainer('a')):
-        if link.has_attr('href'):
-            if id_string in link['href']:
-                return pd.read_csv(link['href'])
+    for link in BeautifulSoup(response, parse_only=SoupStrainer("a")):
+        if link.has_attr("href"):
+            if id_string in link["href"]:
+                return pd.read_csv(link["href"])
 
 
-def munge_data(df : pd.DataFrame, year: int) -> pd.DataFrame:
+def munge_data(df: pd.DataFrame, year: int) -> pd.DataFrame:
     """
     Aggregates all cause mortality data to the level of interest
     for the analysis.
@@ -57,19 +58,23 @@ def munge_data(df : pd.DataFrame, year: int) -> pd.DataFrame:
 
     """
 
-    group_cols = ['ager12', 'monthdth', 'sex']
-    df_grouped = df.groupby(group_cols).size().reset_index().rename({0 : 'death_count'}, axis=1)
-    df_grouped['year'] = year
+    group_cols = ["ager12", "monthdth", "sex"]
+    df_grouped = (
+        df.groupby(group_cols).size().reset_index().rename({0: "death_count"}, axis=1)
+    )
+    df_grouped["year"] = year
 
     # transform the integer death codes
-    if df_grouped.dtypes['sex'] != 'O':
-        df_grouped['sex'] = df_grouped['sex'].map(lambda x : 'M' if x == 1 else 'F')
+    if df_grouped.dtypes["sex"] != "O":
+        df_grouped["sex"] = df_grouped["sex"].map(lambda x: "M" if x == 1 else "F")
 
     return df_grouped
 
+
 def path_to_cache() -> Path:
-    """Path to the data cache """
-    return Path(__file__).parents[0].joinpath('cache')
+    """Path to the data cache"""
+    return Path(__file__).parents[0].joinpath("cache")
+
 
 def get_yearly_mortality_data(year: int) -> pd.DataFrame:
     """
@@ -92,7 +97,7 @@ def get_yearly_mortality_data(year: int) -> pd.DataFrame:
     except FileNotFoundError:
         data = download_year_mortality_data(year)
         data_munged = munge_data(data, year)
-        del data # deletes this from memory to avoid overflow issues
+        del data  # deletes this from memory to avoid overflow issues
         data_munged.to_pickle(file_path)
         return data_munged
 
